c374f940fd9ccf7e52fed8aaf38cc8a6935918b7
Maycon Jardim <mayconsmill@gmail.com>
2022-10-04 20:36:59 -0300

    Get movies

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MoviesService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getMovies() {
+    return this.http.get(
+      'https://api.themoviedb.org/3/movie/upcoming?api_key=652d918d4ab12c96abf1bfa0d7908d1f&language=pt-BR'
+    );
+  }
 }
